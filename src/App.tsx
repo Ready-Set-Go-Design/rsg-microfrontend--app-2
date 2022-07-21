9f4d9fd9b2ec0ce78216f0b89bab25a038e3f714
Brian Hogg <brian@hoggworks.com>
2022-07-21 11:33:02 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Observable } from "windowed-observable";
+
+interface ObservableMessage {
+  target: string;
+  action: string;
+  value?: any;
+}
+
+const Wrapper = styled.div`
+  background-color: #d5d5d5;
+  padding: 20px;
+  margin: 20px;
+`;
+
+const observable = new Observable("mfe-observable");
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [app1Count, setApp1Count] = useState(0);
+  const [messageFromContainer, setMessageFromContainer] = useState<
+    string | null
+  >(null);
+
+  const handleObservableMessage = (
+    message: ObservableMessage,
+    app1Count: number
+  ) => {
+    if (message.target !== "app2") return;
+
+    switch (message.action) {
+      case "increment":
+        setApp1Count(app1Count + 1);
+        break;
+      case "container_message":
+        setMessageFromContainer(message.value);
+    }
+  };
+  useEffect(() => {
+    observable.subscribe((e) => handleObservableMessage(e, app1Count));
+
+    return () => {
+      observable.unsubscribe((e) => handleObservableMessage(e, app1Count));
+    };
+  }, [app1Count]);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+    observable.publish({ action: "increment", target: "app1" });
+  };
+
+  return (
+    <Wrapper>
+      This is the 'app 2' microfrontend.
+      <button onClick={() => incrementCount()}>
+        You've clicked this button {count} time{count === 1 ? "" : "s"}.
+      </button>
+      <p>
+        App 1 has been clicked {app1Count} time{app1Count === 1 ? "" : "s"}.
+      </p>
+      {messageFromContainer && (
+        <p>
+          Message from Container: <em>{messageFromContainer}</em>
+        </p>
+      )}
+    </Wrapper>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.renderApp2 = (containerId, history) => {
+  const root = ReactDOM.createRoot(
+    containerId
+      ? document.getElementById(containerId)
+      : document.getElementById("root")
+  );
+  root.render(<App history={history} />);
+  // unregister();
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.unmountApp2 = (containerId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
